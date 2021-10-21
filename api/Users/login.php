bdfefa7acf79dd02ddc21326972b64c1b3a9834f
dahrif <dahri.fatima.dev@gmail.com>
2021-10-21 11:02:04 +0100

    prototype session

--- api/Users/login.php
+++ api/Users/login.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 // include database and object files
 include_once '../config/database.php';
 include_once '../objects/user.php';
@@ -11,15 +12,19 @@ $db = $database->getConnection();
 // prepare user object
 $user = new users ($db);
 // set ID property of user to be edited
-$user->email = isset($_POST['email']) ? $_POST['email'] : die();
-$user->password = base64_encode(isset($_POST['password']) ? $_POST['password'] : die());
+$user->email = isset($_POST['email']) ? $_SESSION['email']=$_POST['email'] : die();
+$user->password = base64_encode(isset($_POST['password']) ? $_SESSION['password']= $_POST['password'] : die());
+
+if (isset($_POST['password'])){
+    $email = $_SESSION['email'];
+    $pass = $_SESSION['password'];
+}
 // read the details of user to be edited
 $stmt = $user->login();
 if($stmt->rowCount() > 0){
     // get retrieved row
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    header('location:/../login.html');
-
+    echo "hello $email";
 }
 
 else{
