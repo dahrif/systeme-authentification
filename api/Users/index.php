3a2350c03ffc0f6a7d304320d2f632b14d149239
dahrif <dahri.fatima.dev@gmail.com>
2021-10-22 14:53:34 +0100

    save

--- api/Users/index.php
+++ api/Users/index.php
@@ -3,7 +3,7 @@ session_start();
 
 
 if (!isset($_SESSION['username'])){
-header("location: login.php");
+header("location: ../../login.html");
 }
 
 ?>
@@ -12,15 +12,21 @@ header("location: login.php");
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Title</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
 </head>
 <body>
     
     <?php
-     echo "<div class='text-center'><h3>hello" .$_SESSION['username']. "</h3> <a class='btn btn-danger' href='logout.php'>Deconnection</a></div>";
+     echo "<div class='text-center user'><h3>Coucou <span class='username'>" .$_SESSION['username']. "</span> </h3> <a class='btn btn-danger' href='logout.php'>Deconnection</a></div>";
+
+    //  "<div class='text-center user'><h3>Bonjour <span class='username'> $username </span>  </h3>
+    // <a class='btn btn-danger' href='logout.php'>Déxonnexion</a></div>";
      ?>
 
 </body>
